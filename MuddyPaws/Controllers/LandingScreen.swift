0c6ee830d05f4d259409116cf2ef5fc3c89ee294
Gurinder Singh <manivirk07@gmail.com>
2021-07-25 20:35:02 -0400

    Create UI of landing screen with different options of sign in and also for user sign up.

---
+++ MuddyPaws/Controllers/LandingScreen.swift
@@ -0,0 +1,28 @@
+//
+//  LandingScreen.swift
+//  MuddyPaws
+//
+//  Created by Gurinder Singh on 25/07/21.
+//  Copyright © 2021 TechGeeks. All rights reserved.
+//
+
+import UIKit
+
+class LandingScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
