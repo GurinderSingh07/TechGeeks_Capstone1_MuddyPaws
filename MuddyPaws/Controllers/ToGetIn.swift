8c81d0b0cc476cd0b49ab0fbb62c868d51c50b11
Gurinder Singh <manivirk07@gmail.com>
2021-07-26 02:14:13 -0400

    Enable multiple selection of options in GetIn controller to give information of home access to provider.

--- MuddyPaws/Controllers/ToGetIn.swift
+++ MuddyPaws/Controllers/ToGetIn.swift
@@ -9,15 +9,37 @@
 import UIKit
 
 class ToGetIn: UIViewController {
-
     // MARK:- IBOutlets
-       @IBOutlet weak var imgHiddenKey: UIImageView!
-       @IBOutlet weak var imgDoorMen: UIImageView!
-       @IBOutlet weak var imgHome: UIImageView!
+    @IBOutlet weak var imgHiddenKey: UIImageView!
+    @IBOutlet weak var imgDoorMen: UIImageView!
+    @IBOutlet weak var imgHome: UIImageView!
     
-    override func viewDidLoad() {
+    // MARK:- ClassVariable
+    var isHiddenKey = false
+    var isKeyWithMen = false
+    var isFamily = false
+    
+    // MARK:- ViewLifeCycle
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        setInitials()
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK:- PrivateMethods
+    func setInitials()
+    {
+        
     }
     
     // MARK:- UIButtons
@@ -26,10 +48,40 @@ class ToGetIn: UIViewController {
     }
     
     @IBAction func tapNext(_ sender: Any) {
-        
+        let checkOut = UIStoryboard(name: "User", bundle: nil).instantiateViewController(withIdentifier: "CheckOut")
+        self.navigationController?.pushViewController(checkOut, animated: true)
     }
     
     @IBAction func tapToGetIn(_ sender: UIButton) {
-        
+        if sender.tag == 0{
+            if isHiddenKey{
+                isHiddenKey = false
+                imgHiddenKey.image = UIImage(imageLiteralResourceName: "unselect-box")
+            }
+            else{
+                isHiddenKey = true
+                imgHiddenKey.image = UIImage(imageLiteralResourceName: "select-box")
+            }
+        }
+        else if sender.tag == 1{
+            if isKeyWithMen{
+                isKeyWithMen = false
+                imgDoorMen.image = UIImage(imageLiteralResourceName: "unselect-box")
+            }
+            else{
+                isKeyWithMen = true
+                imgDoorMen.image = UIImage(imageLiteralResourceName: "select-box")
+            }
+        }
+        else{
+            if isFamily{
+                isFamily = false
+                imgHome.image = UIImage(imageLiteralResourceName: "unselect-box")
+            }
+            else{
+                isFamily = true
+                imgHome.image = UIImage(imageLiteralResourceName: "select-box")
+            }
+        }
     }
 }
